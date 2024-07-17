09e09038706542b7d6b68a849f9dada9b55e3f82
mahmoud11350 <mahmoudsalah11350@gmail.com>
2024-07-17 13:26:19 +0300

    fixing primitave type

--- app/customer/[id]/transactions/new/page.tsx
+++ app/customer/[id]/transactions/new/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { createNewTransaction } from "@/lib/actions";
 
-const NewTransaction = ({ params }: { params: { id: String } }) => {
+const NewTransaction = ({ params }: { params: { id: string } }) => {
   return (
     <form
       className="w-1/4 space-y-6 mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-xl p-4"
